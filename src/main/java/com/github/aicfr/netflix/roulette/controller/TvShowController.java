b55cb5d4cb90aa554899a5852b6b4d7863995c09
Kevin Heraud <kevin.heraud@atos.net>
2020-11-25 08:17:01 +0100

    refactor()

--- src/main/java/com/github/aicfr/netflix/roulette/controller/TvShowController.java
+++ src/main/java/com/github/aicfr/netflix/roulette/controller/TvShowController.java
@@ -6,8 +6,10 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
 
 @Path("shows")
 public class TvShowController {
@@ -16,10 +18,10 @@ public class TvShowController {
 
     @GET
     @Path("")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     public Response get() {
         return Response
-                .status(Response.Status.OK)
+                .status(OK)
                 .entity(service.get())
                 .build();
     }
